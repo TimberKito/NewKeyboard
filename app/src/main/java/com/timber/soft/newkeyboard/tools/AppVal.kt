427b1184f69b404d07d3d9ca9111caee09fa6d42
LUX-Timber <timberkitowang@gmail.com>
2024-04-03 16:57:48 +0800

    tag01

---
+++ app/src/main/java/com/timber/soft/newkeyboard/tools/AppVal.kt
@@ -0,0 +1,32 @@
+package com.timber.soft.newkeyboard.tools
+
+object AppVal {
+    const val SHIFT_NUMBER = -300
+    const val SHIFT_SYMBOL = -301
+    const val SHARE_NAME = "sp_name"
+    const val KEY_ALL_PATH = "all_path"
+    const val xml0 = 24
+    const val xml1 = 25
+    const val xml2 = 26
+    const val Shift_B_lo = 2
+    const val Shift_S = 0
+    const val Shift_B = 1
+
+    val title_fun_Bg = "btn_keyboard_key_functional_normal.9.png"
+    val title_del_ic= "sym_keyboard_delete_normal.png"
+    val title_shitf_ic = "sym_keyboard_shift.png"
+    val title_func_bg_press = "btn_keyboard_key_functional_pressed.9.png"
+    val title_nor_Bg = "btn_keyboard_key_normal_normal.9.png"
+    val res_path = "/res"
+    val parent_path = "/drawable-xhdpi-v4/"
+    val xx_path = "/drawable-xxhdpi-v4/"
+    val color_path = "/colors.xml"
+    val title_shitf_ic_lock  = "sym_keyboard_shift_locked.png"
+    val title_bg = "keyboard_background.jpg"
+    val title_color = "key_text_color_normal"
+    val title_nor_Bg_press = "btn_keyboard_key_normal_pressed.9.png"
+    val title_sp_Bg = "btn_keyboard_spacekey_normal_normal.9.png"
+    val title_sp_Bg_press = "btn_keyboard_spacekey_normal_pressed.9.png"
+    const  val KEY_POS: String = "key_pos"
+    const val KEYTheme = "key_theme"
+}
